0ebd66c3ddcd105b49fc97dd4645b59225669593
manhtdd <manh.td120901@gmail.com>
2024-05-07 10:16:19 +0700

    Add Lab Image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { graphql } from "gatsby";
+import Slider from "react-slick";
 import Layout from "../components/layout";
 import HeroHeader from "../components/heroHeader";
 import { useThemeContext } from "../contexts/theme-context";
 import HelmetWrapper from "../components/helmetWrapper";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const IndexPage = ({
   data: {
@@ -14,10 +17,28 @@ const IndexPage = ({
 }) => {
   const { theme } = useThemeContext();
 
+  // Slider settings
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 3000,
+  };
+
+  // Render lab images (for the slider)
+  const LabImages = home.labImage[theme].map((imgPath) => (
+    <div key={imgPath} className="image-container">
+      <img src={imgPath} alt={imgPath} style={{ width: "100%" }} />
+    </div>
+  ));
+
   // Render collaborator images
   const CollaboratorImages = home.collaborators[theme].map((imgPath) => (
     <div key={imgPath} className="image-container funder">
-      <img src={imgPath} alt={imgPath} />
+      <img src={imgPath} alt={imgPath} style={{ width: "100%" }} />
     </div>
   ));
 
@@ -25,8 +46,14 @@ const IndexPage = ({
     <Layout>
       <HelmetWrapper />
       <HeroHeader />
+      <h2>Research</h2>
+      <div className="outer-container">
+        <Slider {...sliderSettings}>{LabImages}</Slider>
+      </div>
       <h2>Collaborators</h2>
-      <div className="grids outer-container small small-on-mobile">{CollaboratorImages}</div>
+      <div className="grids outer-container small small-on-mobile">
+        {CollaboratorImages}
+      </div>
     </Layout>
   );
 };
@@ -43,6 +70,10 @@ export const pageQuery = graphql`
           description
           interests
           collaborators {
+            dark
+            light
+          }
+          labImage {
             dark
             light
           }
