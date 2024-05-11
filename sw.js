98c02ab2d746c3da02ad3397194ba59ec87df81d
thanhlecongg <43113794+thanhlecongg@users.noreply.github.com>
2024-05-11 03:11:53 +0000

    Deploying to gh-pages from @ RISE-BKAI/RISE-BKAI.github.io@2b28514e04d34241036e4219ae575a9dd12e5e9c 🚀

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-0fc578e8b3d99d896074.js"
+    "url": "webpack-runtime-5d3572e34887eaabff7d.js"
   },
   {
     "url": "styles.d67981ad1f0147735e98.css"
@@ -46,7 +46,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2f4255e1e4c3aaede1c7d36f813ed49f"
+    "revision": "aefdcfcf97c355979507aec00f18c029"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-241020a1cf7d0f317bbe.js"
